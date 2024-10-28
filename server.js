aec4f710c3d4bf9e60bc10bbf348033a26650715
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-10-28 22:50:56 +0530

    updated

--- server.js
+++ server.js
@@ -7,21 +7,20 @@ import routes from './routes/index.js';
 
 // Load environment variables
 dotenv.config();
-
 // Connect to the Database
 connectDB();
-
 const app = express();
-
 // Middleware
 app.use(express.json());
 app.use(cors());
-
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // Routes
 app.use('/api', routes);
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
