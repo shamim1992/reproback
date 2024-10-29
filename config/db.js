a86a59be544631276ef62d8e2ce3a6a92690cf4a
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-10-29 09:59:55 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect('mongodb+srv://editorneportal:*meghalaya1@cluster0.0kqy5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
